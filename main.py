168a536b9ca3c28d127dd923f3f99d2b652105dd
phuon-nam-nguyen <phuong-nam.nguyen@edu.esiee.fr>
2025-09-19 09:54:47 +0000

    exercice nombre premier

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from math import sqrt
 #### Fonction secondaire
 
 
-def isprime(p):
-
-    # votre code ici
-
-    pass
+def isprime(p:int)->bool:
+    """Indique si le nombre choisie est un nombre premier.
+    Args:
+        p : nombre entier.
+    Returns :
+        bool.
+    """
+    if p < 2: #les nombre <2 ne sont pas premiers
+        return False
+    for i in range (2,p):
+        if p % i ==0:
+            return False
+    return True
 
 #### Fonction principale
 
@@ -16,7 +24,7 @@ def main():
 
     # vos appels à la fonction secondaire ici
 
-    for n in range(100):
+    for n in range(2,100):
         if isprime(n):
             print(n, end=", ")
 
